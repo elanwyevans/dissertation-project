f5022ef2f16dbffbae7aa82997308d2e3447f06d
Elanwy Evans <elanwy.evans@dvla.gov.uk>
2023-12-02 21:25:28 +0000

    changed imageAnalysis lambda to store analysed image labels in dynamodb when post request hits 'analysis' endpoint

--- imageAnalysis.js
+++ imageAnalysis.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const ImageAnalyser = require('./lib/imageAnalyser');
+const AWS = require("aws-sdk");
 const BUCKET_NAME = "serverlessexampledewi"
 
 /**
  Analyse an image on S3 using bucket and image name
  */
-module.exports.imageAnalysis = (event, context, callback) => {
+module.exports.imageAnalysis = (event) => {
     const data = JSON.parse(event.body);
 
     const s3Config = {
@@ -16,19 +17,31 @@ module.exports.imageAnalysis = (event, context, callback) => {
 
     return ImageAnalyser
         .getImageLabels(s3Config)
-        .then((labels) => {
-            //TODO write to dynamo results of Rekognition for image
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify({ Labels: labels }),
+        // if labels are returned from getImageLabels method (via resolve object), will execute this block
+        .then(async (labels) => {
+            //write to dynamo results of Rekognition for image
+            const dynamoDb = new AWS.DynamoDB.DocumentClient();
+            const putParams = {
+                TableName: process.env.DYNAMODB_PROCESSED_IMAGE_LABEL_TABLE,
+                Item: {
+                    primary_key: data.imageName,
+                    labels: JSON.stringify(labels),
+                },
             };
-            callback(null, response);
+            await dynamoDb.put(putParams).promise();
+
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({Labels: labels}),
+            };
         })
+        // if no labels are returned from getImageLabels method (via reject object), will execute this block
         .catch((error) => {
-            callback(null, {
+            return {
                 statusCode: error.statusCode || 501,
                 headers: { 'Content-Type': 'text/plain' },
                 body: error.message || 'Internal server error',
-            });
+            }
         });
 };
